8e94d60ee6865ea65f6b3b559d468a464298c61d
ehap.sadiq@outlook.com <ehap.sadiq@outlook.com>
2025-07-25 22:21:52 +0300

    Add PatientService and IPatientService interface
    Created PatientService.cs to implement patient management methods including add, remove, update, and retrieval with error handling. Defined IPatientService interface to outline the contract for these operations.

---
+++ MediCare Hub/Services/Implementation/PatientService.cs
@@ -0,0 +1,56 @@
+﻿using MediCare_Hub.Data;
+using MediCare_Hub.Models;
+using MediCare_Hub.Services.Interfaces;
+
+namespace MediCare_Hub.Services.Implementation
+{
+    public class PatientService : IPatientService
+    {
+        private readonly MediCareDbContext _context;
+        public PatientService(MediCareDbContext context)
+        {
+            _context = context;
+        }
+        #region Methods
+        public void AddPatient(Patient patient)
+        { 
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+        }
+
+        public void RemovePatient(int id)
+        {
+            Patient patient = GetPatientById(id);
+            if (patient != null)
+            {
+                _context.Patients.Remove(patient);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Patient with ID {id} not found.");
+            }
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            Patient existingPatient = GetPatientById(patient.Id);
+            if (existingPatient != null)
+            {
+                existingPatient.Name = patient.Name;
+                existingPatient.Digonosis = patient.Digonosis;
+                existingPatient.Address = patient.Address;
+                existingPatient.HospitalId = patient.HospitalId;
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Patient with ID {patient.Id} not found.");
+            }
+        }
+        public List<Patient> GetAllPatients() => _context.Patients.ToList();
+        public Patient GetPatientById(int id) => _context.Patients.FirstOrDefault(p => p.Id == id);
+
+
+        #endregion
+    }
+}
---
+++ MediCare Hub/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using MediCare_Hub.Models;
+
+namespace MediCare_Hub.Services.Interfaces
+{
+    public interface IPatientService
+    {
+        public List<Patient> GetAllPatients();
+        public Patient GetPatientById(int id);
+        public void AddPatient(Patient patient);
+        public void UpdatePatient(Patient patient);
+        public void RemovePatient(int id);
+
+
+    }
+}
